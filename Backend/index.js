7f1b568523be85a90980f45930db9dbe7cd0d0e1
vikash chand <vikash.chand2020@vitstudent.ac.in>
2024-08-03 00:45:01 +0530

    aded url changes

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://echotext.vercel.app',
   credentials: true,
   optionSuccessStatus: 200,
 };
